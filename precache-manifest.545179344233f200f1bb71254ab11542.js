6db8239e66588a41aa761fb69bbf1e76a7b35483
Yacine <Yacinemangaka@gmail.com>
2020-10-07 14:32:54 +0100

    Updates

---
+++ precache-manifest.545179344233f200f1bb71254ab11542.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c16299f4d01fda7ff6328ce46a29435",
+    "url": "/FriendsQuotesGame/index.html"
+  },
+  {
+    "revision": "a3aca655577f1427f532",
+    "url": "/FriendsQuotesGame/static/css/main.1db19099.chunk.css"
+  },
+  {
+    "revision": "70763994b29a6ed663e4",
+    "url": "/FriendsQuotesGame/static/js/2.4eabbe6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FriendsQuotesGame/static/js/2.4eabbe6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3aca655577f1427f532",
+    "url": "/FriendsQuotesGame/static/js/main.b1c16f34.chunk.js"
+  },
+  {
+    "revision": "c57bc381e612451470b4",
+    "url": "/FriendsQuotesGame/static/js/runtime-main.8074728c.js"
+  },
+  {
+    "revision": "800a515b3f3fcc8e009421066a4982c9",
+    "url": "/FriendsQuotesGame/static/media/chandler.800a515b.jpg"
+  },
+  {
+    "revision": "13464d4795c51e3685ddf4c07f4f4fe5",
+    "url": "/FriendsQuotesGame/static/media/gunther.13464d47.jpg"
+  },
+  {
+    "revision": "f81386c572f6457e97ec516b8cb036bd",
+    "url": "/FriendsQuotesGame/static/media/janice.f81386c5.jpg"
+  },
+  {
+    "revision": "ee7cff76dff21f445614c3126f1c13ad",
+    "url": "/FriendsQuotesGame/static/media/joey.ee7cff76.jpg"
+  },
+  {
+    "revision": "7abc9203c9653ac7e58a589b175232f4",
+    "url": "/FriendsQuotesGame/static/media/monica.7abc9203.jpg"
+  },
+  {
+    "revision": "4788a2133e9246e4d65c41ea201da25e",
+    "url": "/FriendsQuotesGame/static/media/phoebe.4788a213.jpg"
+  },
+  {
+    "revision": "a590ec601d32f7234c9c397db434a65f",
+    "url": "/FriendsQuotesGame/static/media/rachel.a590ec60.jpg"
+  },
+  {
+    "revision": "25f1a509e75066a5b66f1c41848789b6",
+    "url": "/FriendsQuotesGame/static/media/ross.25f1a509.jpg"
+  }
+]);
